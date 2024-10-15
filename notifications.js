a91f9a4e2627e4c9cc76beaef0b2664c99e4d491
stasisvisor <stasis_visor.02@icloud.com>
2024-10-15 13:06:00 +0700

    Refactored configuration service

---
+++ notifications.js
@@ -0,0 +1,12 @@
+const memoize = (fn) => {
+  const cache = {};
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    return result;
+  };
+};
